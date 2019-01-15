c708ffc9bb29d5d60bbf173462280b73f5fd51f9
Ivan Khoronzhuk <ivan.khoronzhuk@linaro.org>
2019-01-15 12:58:49 +0200

    xdp_sock: fix frame size assigning
    Use shift instead of ^.
    
    Signed-off-by: Ivan Khoronzhuk <ivan.khoronzhuk@linaro.org>

--- xdp_sock.c
+++ xdp_sock.c
@@ -33,7 +33,7 @@
 #define CQ_DESC_NUM	1024
 
 #define FRAME_SHIFT	11
-#define FRAME_SIZE	(2 ^ FRAME_SHIFT)	/* 2 frames per page */
+#define FRAME_SIZE	(1 << FRAME_SHIFT)	/* 2 frames per page */
 #define FRAME_NUM	256	/* number of frames to operate on */
 #define FRAME_HEADROOM	0
 
